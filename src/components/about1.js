810cf19e6a2524dc57d4e042313ffa716d625a3d
alwanar <alwanar210801@gmail.com>
2022-08-05 22:15:50 +0700

    services edited

--- src/components/about1.js
+++ src/components/about1.js
@@ -5,7 +5,7 @@ const about1 = () => {
     return(
         <section className="max-w-[1440px] mx-auto mb-[50px] flex flex-col justify-center items-center gap-y-[50px] gap-x-[50px] md:flex-row md:justify-between">
             <div className="w-full h-full max-h-[600px] flex justify-center md:w-1/2 overflow-hidden md:rounded-br-[64px] md:rounded-tr-[64px] ">
-                <img className="object-cover" src={Home1} alt=""/>
+                <img className="object-cover mx-auto" src={Home1} alt=""/>
             </div>
             <div className="mx-8 md:w-1/2 max-w-[500px] md:mx-0 md:mr-[64px] lg:mr-[100px] md:text-start">
                 <h2 className="text-h2 text-h font-semibold mb-[50px] ">Set up task and schedule with a best features</h2>
