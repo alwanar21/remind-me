842f6a58006ddb53e64df30c30b5587b8136a207
alwanar <alwanar210801@gmail.com>
2022-08-05 20:49:51 +0700

    add header components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+
+    extend: {
+      colors: {
+        'h': '#212121',
+        'p': '#747474',
+        'blue-icons': '#2D76F9',
+      },
+      fontFamily: {
+      'poppins': ['Poppins', 'sans-serif'],
+      },
+      fontSize: {
+        h1: ['64px', {
+          lineHeight: '85px',
+        }],
+        h1m: ['48px', {
+          lineHeight: '85px',
+        }],
+        h2: ['36px', {
+          lineHeight: '65px',
+        }],
+        h3: ['24px', {
+          lineHeight: '40px',
+        }],
+        para: ['18px', {
+          lineHeight: '36px',
+        }],
+      },
+    },
   },
   plugins: [],
 }
